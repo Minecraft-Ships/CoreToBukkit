45ee41a041483bc7f7e20cbe6998dc6f92c3a4cb
mosemister <nekronmoseley@gmail.com>
2022-12-18 18:24:37 +0000

    15.3

--- src/main/java/org/core/implementation/bukkit/entity/living/animal/snapshot/BCowSnapshot.java
+++ src/main/java/org/core/implementation/bukkit/entity/living/animal/snapshot/BCowSnapshot.java
@@ -1,5 +1,7 @@
 package org.core.implementation.bukkit.entity.living.animal.snapshot;
 
+import org.bukkit.entity.Cow;
+import org.bukkit.entity.MushroomCow;
 import org.core.entity.EntitySnapshot;
 import org.core.entity.EntityType;
 import org.core.entity.EntityTypes;
@@ -9,10 +11,12 @@ import org.core.implementation.bukkit.entity.BEntitySnapshot;
 import org.core.implementation.bukkit.entity.living.animal.live.BLiveCow;
 import org.core.implementation.bukkit.world.position.impl.BAbstractPosition;
 import org.core.world.position.impl.sync.SyncExactPosition;
+import org.jetbrains.annotations.Nullable;
 
 public class BCowSnapshot extends BEntitySnapshot<LiveCow> implements CowSnapshot {
 
     private boolean adult;
+    private @Nullable MushroomCow.Variant mooshroomVariant;
 
     public BCowSnapshot(SyncExactPosition position) {
         super(position);
@@ -21,11 +25,21 @@ public class BCowSnapshot extends BEntitySnapshot<LiveCow> implements CowSnapsho
     public BCowSnapshot(LiveCow entity) {
         super(entity);
         this.adult = entity.isAdult();
+        if (!(entity instanceof BLiveCow bImplCow)) {
+            return;
+        }
+        Cow rawCow = bImplCow.getBukkitEntity();
+        if (rawCow instanceof MushroomCow mooshroom) {
+            this.mooshroomVariant = mooshroom.getVariant();
+        }
     }
 
     public BCowSnapshot(CowSnapshot entity) {
         super(entity);
         this.adult = entity.isAdult();
+        if (entity instanceof BCowSnapshot bImplCow) {
+            this.mooshroomVariant = bImplCow.mooshroomVariant;
+        }
     }
 
     @Override
@@ -33,12 +47,18 @@ public class BCowSnapshot extends BEntitySnapshot<LiveCow> implements CowSnapsho
         org.bukkit.Location loc = ((BAbstractPosition<Double>) this.position).toBukkitLocation();
         loc.setPitch((float) this.pitch);
         loc.setYaw((float) this.yaw);
-        org.bukkit.entity.Cow cow = (org.bukkit.entity.Cow) loc
-                .getWorld()
-                .spawnEntity(loc, org.bukkit.entity.EntityType.COW);
+        org.bukkit.entity.Cow cow;
+        if (this.mooshroomVariant == null) {
+            cow = (org.bukkit.entity.Cow) loc.getWorld().spawnEntity(loc, org.bukkit.entity.EntityType.COW);
+        } else {
+            cow = (org.bukkit.entity.Cow) loc.getWorld().spawnEntity(loc, org.bukkit.entity.EntityType.MUSHROOM_COW);
+        }
         BLiveCow coreCow = new BLiveCow(cow);
         this.applyDefaults(coreCow);
         coreCow.setAdult(this.adult);
+        if (cow instanceof MushroomCow mushroomCow) {
+            mushroomCow.setVariant(this.mooshroomVariant);
+        }
         return coreCow;
     }
 
